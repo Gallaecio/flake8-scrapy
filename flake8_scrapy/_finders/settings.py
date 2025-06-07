e7aa09ac29bc0615f6eeafbec6dbc04c76880746
Adrián Chaves <adrian@chaves.io>
2025-06-07 21:34:16 +0200

    Add plugin settings

--- flake8_scrapy/_finders/settings.py
+++ flake8_scrapy/_finders/settings.py
@@ -30,7 +30,7 @@ class SettingInfo:
 # Use this for settings deprecated in Scrapy 2.0.1 or lower.
 MINIMUM_SUPPORTED_SCRAPY_VERSION_OR_LOWER = "2.0.1"
 
-# Grouped by active, deprecated or removed.
+# Grouped by active, deprecated, removed, and plugin-specific.
 # Active settings are sorted as in scrapy.settings.default_settings, while
 # deprecated and removed settings are sorted by the version that deprecated or
 # removed them, from higher to lower.
@@ -242,6 +242,179 @@ SETTINGS = {
         deprecated_version=MINIMUM_SUPPORTED_SCRAPY_VERSION_OR_LOWER,
         deprecation_message="Use METAREFRESH_MAXDELAY instead.",
     ),
+    # scrapy-feedexporter-azure-storage plugin settings, in order of appearance
+    # in https://github.com/scrapy-plugins/scrapy-feedexporter-azure-storage
+    "AZURE_CONNECTION_STRING": SettingInfo(),
+    "AZURE_ACCOUNT_URL_WITH_SAS_TOKEN": SettingInfo(),
+    "AZURE_ACCOUNT_URL": SettingInfo(),
+    "AZURE_ACCOUNT_KEY": SettingInfo(),
+    # scrapy-deltafetch plugin settings, in order of appearance in
+    # https://github.com/scrapy-plugins/scrapy-deltafetch#usage
+    "DELTAFETCH_ENABLED": SettingInfo(),
+    "DELTAFETCH_DIR": SettingInfo(),
+    "DELTAFETCH_RESET": SettingInfo(),
+    # scrapy-feedexporter-dropbox plugin settings, in order of appearance in
+    # https://github.com/scrapy-plugins/scrapy-feedexporter-dropbox
+    "DROPBOX_API_TOKEN": SettingInfo(),
+    # scrapy-frontera plugin settings, in order of appearance in
+    # https://github.com/scrapinghub/scrapy-frontera#usage-and-features
+    "FRONTERA_SCHEDULER_START_REQUESTS_TO_FRONTIER": SettingInfo(),
+    "FRONTERA_SCHEDULER_REQUEST_CALLBACKS_TO_FRONTIER": SettingInfo(),
+    "FRONTERA_SCHEDULER_STATE_ATTRIBUTES": SettingInfo(),
+    "FRONTERA_SCHEDULER_CALLBACK_SLOT_PREFIX_MAP": SettingInfo(),
+    "BACKEND": SettingInfo(),
+    # scrapy-feedexporter-google-drive plugin settings, in order of appearance
+    # in https://github.com/scrapy-plugins/scrapy-feedexporter-google-drive
+    "GDRIVE_SERVICE_ACCOUNT_CREDENTIALS_JSON": SettingInfo(),
+    # scrapy-feedexporter-google-sheets plugin settings, in order of appearance
+    # in https://github.com/scrapy-plugins/scrapy-feedexporter-google-sheets
+    "GOOGLE_CREDENTIALS": SettingInfo(),
+    # hcf-backend plugin settings, in order of appearance in
+    # https://github.com/scrapinghub/hcf-backend/blob/master/hcf_backend/backend.py
+    "HCF_CONSUMER_MAX_REQUESTS": SettingInfo(),
+    "HCF_CONSUMER_MAX_BATCHES": SettingInfo(),
+    "MAX_NEXT_REQUESTS": SettingInfo(),
+    "HCF_AUTH": SettingInfo(),
+    "HCF_PROJECT_ID": SettingInfo(),
+    "HCF_PRODUCER_FRONTIER": SettingInfo(),
+    "HCF_PRODUCER_SLOT_PREFIX": SettingInfo(),
+    "HCF_PRODUCER_NUMBER_OF_SLOTS": SettingInfo(),
+    "HCF_PRODUCER_BATCH_SIZE": SettingInfo(),
+    "HCF_CONSUMER_FRONTIER": SettingInfo(),
+    "HCF_CONSUMER_SLOT": SettingInfo(),
+    "HCF_CONSUMER_DONT_DELETE_REQUESTS": SettingInfo(),
+    "HCF_CONSUMER_DELETE_BATCHES_ON_STOP": SettingInfo(),
+    # scrapy-incremental plugin settings, in order of appearance in
+    # https://github.com/scrapy-plugins/scrapy-incremental
+    "SCRAPYCLOUD_API_KEY": SettingInfo(),
+    "SCRAPYCLOUD_PROJECT_ID": SettingInfo(),
+    "INCREMENTAL_PIPELINE_ITEM_UNIQUE_FIELD": SettingInfo(),
+    "INCREMENTAL_PIPELINE_BATCH_SIZE": SettingInfo(),
+    # scrapy-feedexporter-onedrive plugin settings, in order of appearance in
+    # https://github.com/scrapy-plugins/scrapy-feedexporter-onedrive
+    "ONEDRIVE_ACCESS_TOKEN": SettingInfo(),
+    # scrapy-playwright plugin settings, in order of appearance in
+    # https://github.com/scrapy-plugins/scrapy-playwright#supported-settings
+    "PLAYWRIGHT_BROWSER_TYPE": SettingInfo(),
+    "PLAYWRIGHT_LAUNCH_OPTIONS": SettingInfo(),
+    "PLAYWRIGHT_CDP_URL": SettingInfo(),
+    "PLAYWRIGHT_CONNECT_URL": SettingInfo(),
+    "PLAYWRIGHT_CONNECT_KWARGS": SettingInfo(),
+    "PLAYWRIGHT_CONTEXTS": SettingInfo(),
+    "PLAYWRIGHT_MAX_CONTEXTS": SettingInfo(),
+    "PLAYWRIGHT_DEFAULT_NAVIGATION_TIMEOUT": SettingInfo(),
+    "PLAYWRIGHT_PROCESS_REQUEST_HEADERS": SettingInfo(),
+    "PLAYWRIGHT_RESTART_DISCONNECTED_BROWSER": SettingInfo(),
+    "PLAYWRIGHT_MAX_PAGES_PER_CONTEXT": SettingInfo(),
+    "PLAYWRIGHT_ABORT_REQUEST": SettingInfo(),
+    # scrapy-poet plugin settings, in order of appearance in
+    # https://scrapy-poet.readthedocs.io/en/stable/settings.html
+    "SCRAPY_POET_CACHE": SettingInfo(),
+    "SCRAPY_POET_CACHE_ERRORS": SettingInfo(),
+    "SCRAPY_POET_DISCOVER": SettingInfo(),
+    "SCRAPY_POET_OVERRIDES": SettingInfo(),
+    "SCRAPY_POET_PROVIDERS": SettingInfo(),
+    "SCRAPY_POET_REQUEST_FINGERPRINTER_BASE_CLASS": SettingInfo(),
+    "SCRAPY_POET_RULES": SettingInfo(),
+    "SCRAPY_POET_TESTS_ADAPTER": SettingInfo(),
+    "SCRAPY_POET_TESTS_DIR": SettingInfo(),
+    # scrapy-redis plugin settings, in order of appearance in
+    # https://github.com/rmax/scrapy-redis/wiki/Usage
+    "SCHEDULER_SERIALIZER": SettingInfo(),
+    "SCHEDULER_PERSIST": SettingInfo(),
+    "SCHEDULER_QUEUE_CLASS": SettingInfo(),
+    "SCHEDULER_IDLE_BEFORE_CLOSE": SettingInfo(),
+    "REDIS_ITEMS_KEY": SettingInfo(),
+    "REDIS_ITEMS_SERIALIZER": SettingInfo(),
+    "REDIS_HOST": SettingInfo(),
+    "REDIS_PORT": SettingInfo(),
+    "REDIS_URL": SettingInfo(),
+    "REDIS_PARAMS": SettingInfo(),
+    "REDIS_START_URLS_AS_SET": SettingInfo(),
+    "REDIS_START_URLS_KEY": SettingInfo(),
+    "REDIS_ENCODING": SettingInfo(),
+    # scrapyrt plugin settings, in order of appearance in
+    # https://scrapyrt.readthedocs.io/en/latest/api.html#available-settings
+    "SERVICE_ROOT": SettingInfo(),
+    "CRAWL_MANAGER": SettingInfo(),
+    "RESOURCES": SettingInfo(),
+    "LOG_DIR": SettingInfo(),
+    "TIMEOUT_LIMIT": SettingInfo(),
+    "DEBUG": SettingInfo(),
+    "PROJECT_SETTINGS": SettingInfo(),
+    # scrapy-settings-log plugin settings, in order of appearance in
+    # https://github.com/scrapy-plugins/scrapy-settings-log
+    "SETTINGS_LOGGING_ENABLED": SettingInfo(),
+    "SETTINGS_LOGGING_REGEX": SettingInfo(),
+    "SETTINGS_LOGGING_INDENT": SettingInfo(),
+    "MASKED_SENSITIVE_SETTINGS_ENABLED": SettingInfo(),
+    # scrapy-feedexporter-sftp plugin settings, in order of appearance in
+    # https://github.com/scrapy-plugins/scrapy-feedexporter-sftp
+    "FEED_STORAGE_SFTP_PKEY": SettingInfo(),
+    # spidermon plugin settings, in order of appearance in
+    # https://spidermon.readthedocs.io/en/latest/settings.html
+    "SPIDERMON_ENABLED": SettingInfo(),
+    "SPIDERMON_EXPRESSIONS_MONITOR_CLASS": SettingInfo(),
+    "SPIDERMON_PERIODIC_MONITORS": SettingInfo(),
+    "SPIDERMON_SPIDER_CLOSE_MONITORS": SettingInfo(),
+    "SPIDERMON_SPIDER_CLOSE_EXPRESSION_MONITORS": SettingInfo(),
+    "SPIDERMON_SPIDER_OPEN_MONITORS": SettingInfo(),
+    "SPIDERMON_SPIDER_OPEN_EXPRESSION_MONITORS": SettingInfo(),
+    "SPIDERMON_ENGINE_STOP_MONITORS": SettingInfo(),
+    "SPIDERMON_ENGINE_STOP_EXPRESSION_MONITORS": SettingInfo(),
+    "SPIDERMON_ADD_FIELD_COVERAGE": SettingInfo(),
+    "SPIDERMON_FIELD_COVERAGE_SKIP_NONE": SettingInfo(),
+    "SPIDERMON_LIST_FIELDS_COVERAGE_LEVELS": SettingInfo(),
+    "SPIDERMON_DICT_FIELDS_COVERAGE_LEVELS": SettingInfo(),
+    "SPIDERMON_MONITOR_SKIPPING_RULES": SettingInfo(),
+    # scrapy-zyte-api plugin settings, in order of appearance in
+    # https://scrapy-zyte-api.readthedocs.io/en/latest/reference/settings.html
+    "ZYTE_API_AUTO_FIELD_STATS": SettingInfo(),
+    "ZYTE_API_AUTOMAP_PARAMS": SettingInfo(),
+    "ZYTE_API_BROWSER_HEADERS": SettingInfo(),
+    "ZYTE_API_COOKIE_MIDDLEWARE": SettingInfo(),
+    "ZYTE_API_DEFAULT_PARAMS": SettingInfo(),
+    "ZYTE_API_ENABLED": SettingInfo(),
+    "ZYTE_API_EXPERIMENTAL_COOKIES_ENABLED": SettingInfo(),
+    "ZYTE_API_FALLBACK_HTTP_HANDLER": SettingInfo(),
+    "ZYTE_API_FALLBACK_HTTPS_HANDLER": SettingInfo(),
+    "ZYTE_API_FALLBACK_REQUEST_FINGERPRINTER_CLASS": SettingInfo(),
+    "ZYTE_API_KEY": SettingInfo(),
+    "ZYTE_API_LOG_REQUESTS": SettingInfo(),
+    "ZYTE_API_LOG_REQUESTS_TRUNCATE": SettingInfo(),
+    "ZYTE_API_MAX_COOKIES": SettingInfo(),
+    "ZYTE_API_MAX_REQUESTS": SettingInfo(),
+    "ZYTE_API_PRESERVE_DELAY": SettingInfo(),
+    "ZYTE_API_PROVIDER_PARAMS": SettingInfo(),
+    "ZYTE_API_REFERRER_POLICY": SettingInfo(),
+    "ZYTE_API_RETRY_POLICY": SettingInfo(),
+    "ZYTE_API_SESSION_CHECKER": SettingInfo(),
+    "ZYTE_API_SESSION_ENABLED": SettingInfo(),
+    "ZYTE_API_SESSION_LOCATION": SettingInfo(),
+    "ZYTE_API_SESSION_MAX_BAD_INITS": SettingInfo(),
+    "ZYTE_API_SESSION_MAX_BAD_INITS_PER_POOL": SettingInfo(),
+    "ZYTE_API_SESSION_MAX_CHECK_FAILURES": SettingInfo(),
+    "ZYTE_API_SESSION_MAX_ERRORS": SettingInfo(),
+    "ZYTE_API_SESSION_PARAMS": SettingInfo(),
+    "ZYTE_API_SESSION_POOL_SIZE": SettingInfo(),
+    "ZYTE_API_SESSION_POOL_SIZES": SettingInfo(),
+    "ZYTE_API_SESSION_QUEUE_MAX_ATTEMPTS": SettingInfo(),
+    "ZYTE_API_SESSION_QUEUE_WAIT_TIME": SettingInfo(),
+    "ZYTE_API_SKIP_HEADERS": SettingInfo(),
+    "ZYTE_API_TRANSPARENT_MODE": SettingInfo(),
+    "ZYTE_API_USE_ENV_PROXY": SettingInfo(),
+    # scrapy-zyte-smartproxy plugin settings, in order of appearance in
+    # https://scrapy-zyte-smartproxy.readthedocs.io/en/latest/settings.html
+    "ZYTE_SMARTPROXY_APIKEY": SettingInfo(),
+    "ZYTE_SMARTPROXY_URL": SettingInfo(),
+    "ZYTE_SMARTPROXY_MAXBANS": SettingInfo(),
+    "ZYTE_SMARTPROXY_DOWNLOAD_TIMEOUT": SettingInfo(),
+    "ZYTE_SMARTPROXY_PRESERVE_DELAY": SettingInfo(),
+    "ZYTE_SMARTPROXY_DEFAULT_HEADERS": SettingInfo(),
+    "ZYTE_SMARTPROXY_BACKOFF_STEP": SettingInfo(),
+    "ZYTE_SMARTPROXY_BACKOFF_MAX": SettingInfo(),
+    "ZYTE_SMARTPROXY_FORCE_ENABLE_ON_HTTP_CODES": SettingInfo(),
+    "ZYTE_SMARTPROXY_KEEP_HEADERS": SettingInfo(),
 }
 
 HARDCODED_SUGGESTIONS = {
